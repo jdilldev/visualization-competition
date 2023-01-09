e00277ec6b2fb5e6df114ce168d36fca7a23e0c6
Jasmine Dillard <jdilldev@gmail.com>
2023-01-09 15:59:50 -0500

    layout

--- world-summit-2023/app/layout.tsx
+++ world-summit-2023/app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css'
 
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: {
   children: React.ReactNode
-}) {
+}) => {
   return (
     <html lang="en">
       {/*
@@ -12,7 +12,16 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body style={{ padding: '10px', height: '100vh', }} className="bg-black h-screen text-white font-body subpixel-antialiased p-3">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+        {children}
+      </body>
     </html>
   )
 }
+
+export default RootLayout;
+
+//background: 'linear-gradient(129deg, rgba(53,75,71,1) 19%, rgba(70,113,108,1) 50%, rgba(53,75,71,1) 80%)'
--- world-summit-2023/app/page.tsx
+++ world-summit-2023/app/page.tsx
@@ -1,91 +1,36 @@
+"use client"
+
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Grid } from '@nextui-org/react';
+import { Button } from "@nextui-org/react";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type GridProps = {
+  children: React.ReactNode
+  className: string
+}
+
+const themes: string[] = ['1', '2', '3', '4', '5', '6']
+
+const hair = 'hair'
+
+const Home = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Grid className='grid grid-cols-6 grid-rows-8 gap-3 h-full'>
+      <Grid className='row-span-1 col-start-2 col-span-4 border-solid border-2 border-indigo-600'>Summary</Grid>
+      <Grid className='row-span-3 col-start-1 col-span-2 border-solid border-2 border-indigo-600'>Chart I</Grid>
+      <Grid className='row-span-5 col-span-4 border-solid border-2 border-indigo-600'>MapGrid</Grid>
+      <Grid className='row-span-4 col-start-1 col-span-2 border-solid border-2 border-indigo-600'>Chart II</Grid>
+      <Grid className=' row-span-2 col-start-3 col-span-2 border-solid border-2 border-indigo-600'>Chart III</Grid>
+      <Grid className=' row-span-2 col-span-2 col-end-7 border-solid border-2 border-indigo-600'>Chart IV</Grid>
+      <Grid className='col-start-2 col-span-4 border-solid border-2 border-indigo-600'>
+      </Grid>
+    </Grid>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
+
+export default Home
