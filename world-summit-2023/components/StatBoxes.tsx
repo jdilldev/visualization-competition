778f38caa41096c3a05c3b5f137586ed381e5176
Jasmine Dillard <jdilldev@gmail.com>
2023-01-27 13:18:22 -0500

    slight refactor

--- world-summit-2023/components/StatBoxes.tsx
+++ world-summit-2023/components/StatBoxes.tsx
@@ -1,21 +1,31 @@
 import { Grid, } from '@nextui-org/react';
 import { FrameLines } from '@arwes/core';
-import Lightbulb from '../public/icons/brain.svg'
+import InsightIcon from '../public/icons/brain.svg'
 
-const DefaultStatBox = ({ partOne, partTwo }: { partOne: string, partTwo: string }) => {
+type DefaultStatItem = {
+    numeric: string
+    text: string
+}
+
+const defaultStatBoxes: DefaultStatItem[] = [
+    { numeric: '13', text: 'Subregions' },
+    { numeric: '174', text: 'Countries' },
+    { numeric: '6', text: 'Themes' },
+    { numeric: '', text: 'Endless Insights' }
+]
+
+const DefaultStatBox = ({ item }: { item: DefaultStatItem }) => {
     return <FrameLines palette='secondary' className='w-1/4 h-full flex flex-col justify-between' animator={{ animate: false }} hover largeLineWidth={2} smallLineWidth={4} smallLineLength={20}>
-        {partOne !== '' ? <p className='w-12 h-12 font-nebula text-3xl'>{partOne}</p> : <Lightbulb className='w-16 h-12 pb-2 fill-[#9fd0dcb1]' />}
-        <p className='font-nebula lg:whitespace-nowrap text-sm md:text-base'>{partTwo}</p>
+        {item.numeric !== '' ? <p className='w-12 h-12 font-nebula text-3xl'>{item.numeric}</p> : <InsightIcon className='w-16 h-12 pb-2 fill-[#9fd0dcb1]' />}
+        <p className='font-nebula lg:whitespace-nowrap text-sm md:text-base'>{item.text}</p>
     </FrameLines >
 }
 
-export const StatBoxes = () => {
-    return (
-        <Grid className='flex gap-4 justify-between h-[10%] md:h-[12.5%] lg:h-[20%]'>
-            <DefaultStatBox partOne='13' partTwo='Subregions' />
-            <DefaultStatBox partOne='174' partTwo='Countries' />
-            <DefaultStatBox partOne='6' partTwo='Themes' />
-            <DefaultStatBox partOne='' partTwo='Endless Insights' />
 
-        </Grid>)
+const Default = () => <Grid className='flex gap-4 justify-between h-[10%] md:h-[12.5%] lg:h-[20%]'>
+    {defaultStatBoxes.map((item: DefaultStatItem) => <DefaultStatBox item={item} />)}
+</Grid>
+
+export const StatBoxes = () => {
+    return <Default />
 }
